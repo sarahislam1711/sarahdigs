9afe8214477ca97a282deb05148071bb0134b3e7
boussa69 <islamjp69@gmail.com>
2026-01-27 15:36:51 +0200

    Add cookie consent to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import CookieConsent from "@/components/cookie-consent";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Contact from "@/pages/contact";
@@ -92,6 +93,7 @@ function App() {
         <Toaster />
         <ScrollToTop />
         <Router />
+        <CookieConsent />
       </TooltipProvider>
     </QueryClientProvider>
   );
